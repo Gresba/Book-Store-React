66b65687e32440791dcfcefb901fc7b7d462c790
gresba <kimpaul0709@gmail.com>
2022-07-29 08:45:07 -0400

    Added other files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import Navbar from "./components/Navbar.jsx";
 import BookContainer from "./components/BookContainer.jsx";
 import BookPage from "./components/Bookpage.jsx";
-import { Routes, Route, Link } from "react-router-dom";
+import Upload from "./components/Upload.jsx";
+import { Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react"
 
 function App() {
 
     let [ bookData, setBookData ] = useState([])
+    let [ toggle, setToggle ] = useState(false)
 
     useEffect(() => {
-        if(bookData.length === 0)
-        {
             fetch("https://jsonbookapi1.herokuapp.com/api/books")
                 .then(res => res.json())
                 .then(res => setBookData(res))  
-        }
-    }, [])
+    }, [toggle])
 
   return (
     <>
@@ -23,7 +22,8 @@ function App() {
         <main>
             <Routes>
                 <Route path="/" element={<BookContainer data={bookData}/>}/>
-                <Route path="/books/:id" element={<BookPage/>}/>
+                <Route path="/books/:id" element={<BookPage setToggle={setToggle}/>}/>
+                <Route path="/upload" element={<Upload/>}/>
             </Routes>
         </main>
     </>
